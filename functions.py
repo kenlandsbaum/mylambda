6c06db6ff1d26c8a8229858ae9863f8a60dd88d7
kenlandsbaum <158199991+kenlandsbaum@users.noreply.github.com>
2024-04-25 19:39:24 -0500

    Delete functions.py

--- functions.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def make_get_request(url: str):
-    try:
-        res = requests.get(url=url)
-        return res.json()
-    except Exception as ex:
-        return {"error": ex}
